c17488ae2979a32dbf95bb6b632acce79d11c542
warmstrongsr <will.ea.armstrong@gmail.com>
2023-07-10 05:26:14 -0500

    time and space

--- hash_tables.py
+++ hash_tables.py
@@ -12,7 +12,7 @@ Inserting a key/value pair: O(1) on avg.; O(n) in the worse case
 Removing a key/value pair: O(1) on avg.; O(n) in the worse case
 Looking up a key: O(1) on avg.; O(n) in the worse case
 
-The worst-case linear time operations occur when a hash table experiences a lot of collisions, leading to long linked lists internally, which take O(n) time to travers.
+The worst-case linear time operations occur when a hash table experiences a lot of collisions, leading to long linked lists internally, which take O(n) time to traverse.
 
 However, in practice and especially in coding interviews, we typically assume that the hash functions employed by hash tables are so optimized that the collisions are extremely rare and constant-time operations are all but guaranteed.
 """
